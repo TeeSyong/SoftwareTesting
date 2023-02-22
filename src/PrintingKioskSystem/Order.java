6d13dda8c2e9251da91a4e8b88cbac46acfb400d
Elainee610 <elaine610.lau@gmail.com>
2023-02-22 22:19:47 +0800

    yeelin update

--- src/PrintingKioskSystem/Order.java
+++ src/PrintingKioskSystem/Order.java
@@ -2,4 +2,35 @@ package PrintingKioskSystem;
 
 public class Order {
 
+	private String type;
+	private String option;
+	private int quantity;
+	private String addOpt;
+	
+	public Order(String type, String option, int quantity, String addOpt) {
+		this.type = type;
+		this.option = option;
+		this.quantity = quantity;
+		this.addOpt = addOpt;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public String getOption()
+	{
+		return option;
+	}
+	
+	public int getQuantity()
+	{
+		return quantity;
+	}
+	
+	public String getAddOpt()
+	{
+		return addOpt;
+	}
+	
 }
--- src/PrintingKioskSystem/Printing.java
+++ src/PrintingKioskSystem/Printing.java
@@ -1,5 +1,42 @@
 package PrintingKioskSystem;
 
-public class Printing {
+import java.util.Scanner;
 
+public class Printing {
+private static Scanner scanner;
+	
+	public static void main(String[] args) {
+		scanner = new Scanner(System.in);
+		
+		int choice;
+		boolean error;
+		String order,opt;
+		int qtt,addOpt;
+		
+		
+		do {
+			System.out.print("Enter type of order(D-Document/P-Photo): ");
+			order = scanner.nextLine();
+			if(order.equals("D")){
+				System.out.print("Enter option(A-Black & White/B-Colour): ");
+				opt = scanner.nextLine();
+			}
+			else {
+				System.out.print("Enter option(C-Normal/D-Passport): ");
+				opt = scanner.nextLine();
+			}
+			System.out.print("Enter quantity: ");
+			qtt = scanner.nextInt();
+			System.out.println("Additional Option");
+			System.out.println("1. High quality paper");
+			System.out.println("2. Design effect");
+			addOpt = scanner.nextInt();
+			
+			
+		}
+		
+		
+		
+		
+	}
 }
