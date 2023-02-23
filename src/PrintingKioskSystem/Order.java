cac06a3ae87b87166c488d47fc99757f622db7f6
Czooi <126080176+Czooi@users.noreply.github.com>
2023-02-23 21:24:47 +0800

    Update Order.java
    update

--- src/PrintingKioskSystem/Order.java
+++ src/PrintingKioskSystem/Order.java
@@ -6,34 +6,7 @@ public class Order {
 
 	
 	private static Scanner scanner;
-	private static String mainOpt1,mainOpt2, mainOrderType, mainAddOpt;
-	private static int mainQtt1, mainQtt2, mainCheckLoop;
-	private static double mainTotalCharge;
 	
-	public String getMainOpt1(){
-		return mainOpt1;
-	}
-	public String getMainOpt2() {
-		return mainOpt2;
-	}
-	public String getMainOrderType() {
-		return mainOrderType;
-	}
-	public String getMainAddOpt() {
-		return mainAddOpt;
-	}
-	public int getMainQtt1() {
-		return mainQtt1;
-	}
-	public int getMainQtt2() {
-		return mainQtt2;
-	}
-	public int getMainCheckLoop() {
-		return mainCheckLoop;
-	}
-	public double getMainTotalCharge() {
-		return mainTotalCharge;
-	}
 	
 	
 	public static void main(String[] args) {
@@ -47,12 +20,11 @@ public class Order {
 			System.out.print("Enter type of order(D-Document/P-Photo)(E-exit): ");
 			order = scanner.next().toUpperCase();
 			switch(order) {
-			case "D": document(); mainOrderType = "Document"; error = false; break;
-			case "P": photo(); mainOrderType = "Photo"; error = false; break;
+			case "D": document(); error = false; break;
+			case "P": photo(); error = false; break;
 			case "E": error = false;break;
 			default: error = true; break;
 			}
-			
 			
 			if(error) {
 				System.out.println("Invalid choice. Please enter again");
@@ -65,7 +37,6 @@ public class Order {
 	public static void document() {
 		String opt,choice;
 		int bwQtt = 0,colorQtt = 0,ttlQtt;
-		int checkLoop = 0;
 		double totalCharge= 0;
 		Charge ch1 = null, ch2= null;
 		boolean error,er;
@@ -75,10 +46,8 @@ public class Order {
 			System.out.print("Enter option(A-Black & White/B-Colour): ");
 			opt = scanner.next().toUpperCase();
 			if(opt.equals("A")) {
-				mainOpt1 = "Black & White";
 				System.out.print("Enter quantity in black and white: ");
 				bwQtt = scanner.nextInt();
-				mainQtt1 = bwQtt;
 				ch1 = new Charge(opt,bwQtt);
 				
 				do {
@@ -101,10 +70,8 @@ public class Order {
 			}
 			else if(opt.equals("B"))
 			{
-				mainOpt2 = "Colour";
 				System.out.print("Enter quantity in colour: ");
 				colorQtt = scanner.nextInt();
-				mainQtt2 = colorQtt;
 				ch2 = new Charge(opt,colorQtt);
 				
 				do {
@@ -129,9 +96,9 @@ public class Order {
 				System.out.println("Invalid choice. Please enter again.");
 				error=true;
 			}
-			checkLoop++;
+			
 		}while(error);
-		mainCheckLoop = checkLoop;
+
 		//Calculate total quantity for documents
 		ttlQtt = bwQtt + colorQtt;
 		
@@ -150,7 +117,7 @@ public class Order {
 			totalCharge = ch1.calNormalCharge() + ch2.calNormalCharge();
 		}
 		
-		//System.out.println("Total charges: "+ totalCharge);
+		System.out.println("Total charges: "+ totalCharge);
 	
 	}
 	
@@ -158,21 +125,17 @@ public class Order {
 		
 		String opt, choice;
 		int ttlQtt, normalQtt = 0, passportQtt = 0, addOpt;
-		int checkLoop = 0;
 		double totalCharge = 0;
 		Charge ch3 = null, ch4= null, surcharge = null;
 		boolean error, er;
-		
 		
 		do {
 			error=false;
 			System.out.print("Enter option(C-Normal(4R)/D-Passport): ");
 			opt = scanner.next().toUpperCase();
 			if(opt.equals("C")) {
-				mainOpt1 = "Normal(4R)";
 				System.out.print("Enter quantity in normal(4R): ");
 				normalQtt = scanner.nextInt();
-				mainQtt1 = normalQtt;
 				ch3 = new Charge(opt,normalQtt);
 				
 				do {
@@ -184,7 +147,6 @@ public class Order {
 						er=false;
 					}
 					else if(choice.equals("N")) {
-						System.out.println("Proceeding to billing...");
 						er=false;
 					}
 					else {
@@ -195,10 +157,8 @@ public class Order {
 			}
 			else if(opt.equals("D"))
 			{
-				mainOpt2 = "Passport";
 				System.out.print("Enter quantity in passport: ");
 				passportQtt = scanner.nextInt();
-				mainQtt2 = passportQtt;
 				ch4 = new Charge(opt,passportQtt);
 				
 				do {
@@ -210,7 +170,6 @@ public class Order {
 						er=false;
 					}
 					else if(choice.equals("N")) {
-						System.out.println("Proceeding to billing...");
 						er=false;
 					}
 					else {
@@ -223,9 +182,8 @@ public class Order {
 				System.out.println("Invalid choice. Please enter again.");
 				error=true;
 			}
-			checkLoop++;
+			
 		}while(error);
-		mainCheckLoop = checkLoop;
 		
 		//Calculate total quantity for photos
 		ttlQtt = normalQtt + passportQtt;
@@ -241,10 +199,6 @@ public class Order {
 			
 			if(addOpt==1||addOpt==2) {
 				surcharge = new Charge(addOpt,ttlQtt);
-				if(addOpt==1)
-					mainAddOpt="High quality paper";
-				else 
-					mainAddOpt="Design effect";
 				error=false;
 			}
 			else if(addOpt==3) {
@@ -282,8 +236,7 @@ public class Order {
 		}
 		
 		
-		//System.out.println("Total charges: "+ totalCharge);
+		System.out.println("Total charges: "+ totalCharge);
 	}
-	
 	
 }
