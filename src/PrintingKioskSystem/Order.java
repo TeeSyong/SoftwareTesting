7d134713f0c82b217419a29a456ba86b083b97be
Czooi <126080176+Czooi@users.noreply.github.com>
2023-02-23 18:41:25 +0800

    Update Order.java
    update

--- src/PrintingKioskSystem/Order.java
+++ src/PrintingKioskSystem/Order.java
@@ -6,6 +6,12 @@ public class Order {
 
 	
 	private static Scanner scanner;
+	private static String mainOpt1,mainOpt2, mainOrderType;
+	private static int mainQtt1, mainQtt2;
+	private static double mainTotalCharge;
+	static int mainCheckLoop;
+	
+	
 	
 	public static void main(String[] args) {
 		scanner = new Scanner(System.in);
@@ -18,36 +24,38 @@ public class Order {
 			System.out.print("Enter type of order(D-Document/P-Photo)(E-exit): ");
 			order = scanner.next().toUpperCase();
 			switch(order) {
-			case "D": document(); break;
-			case "P": photo(); break;
-			case "E": break;
+			case "D": document(); mainOrderType = "Document"; error = false; break;
+			case "P": photo(); mainOrderType = "Photo"; error = false; break;
+			case "E": error = false;break;
 			default: error = true; break;
 			}
+			
 			
 			if(error) {
 				System.out.println("Invalid choice. Please enter again");
 			}
 			
-		}while(!order.equals("E"));
+		}while(error);
 		
 	}
 	
 	public static void document() {
 		String opt,choice;
-		int ttlQtt,bwQtt,colorQtt;
+		int bwQtt = 0,colorQtt = 0,ttlQtt;
+		int checkLoop = 0;
+		double totalCharge= 0;
 		Charge ch1 = null, ch2= null;
 		boolean error,er;
-		
-		System.out.print("Enter total document to print: ");
-		ttlQtt=scanner.nextInt();
 		
 		do {
 			error=false;
 			System.out.print("Enter option(A-Black & White/B-Colour): ");
 			opt = scanner.next().toUpperCase();
 			if(opt.equals("A")) {
+				mainOpt1 = "Black & White";
 				System.out.print("Enter quantity in black and white: ");
 				bwQtt = scanner.nextInt();
+				mainQtt1 = bwQtt;
 				ch1 = new Charge(opt,bwQtt);
 				
 				do {
@@ -59,6 +67,7 @@ public class Order {
 						er=false;
 					}
 					else if(choice.equals("N")) {
+						System.out.println("Proceeding to billing...");
 						er=false;
 					}
 					else {
@@ -69,8 +78,10 @@ public class Order {
 			}
 			else if(opt.equals("B"))
 			{
+				mainOpt2 = "Colour";
 				System.out.print("Enter quantity in colour: ");
 				colorQtt = scanner.nextInt();
+				mainQtt2 = colorQtt;
 				ch2 = new Charge(opt,colorQtt);
 				
 				do {
@@ -82,6 +93,7 @@ public class Order {
 						er=false;
 					}
 					else if(choice.equals("N")) {
+						System.out.println("Proceeding to billing...");
 						er=false;
 					}
 					else {
@@ -94,39 +106,50 @@ public class Order {
 				System.out.println("Invalid choice. Please enter again.");
 				error=true;
 			}
-			
+			checkLoop++;
 		}while(error);
+		mainCheckLoop = checkLoop;
+		//Calculate total quantity for documents
+		ttlQtt = bwQtt + colorQtt;
 		
+		// this part to check the combination of billing
+		// exp: some buy normal and passport without surcharge, some buy normal with surcharge
+				
+		// != null --> inside particular object got something == people buy the option(normal/passport/addOpt)
+				
+		if(ch1 != null && ch2 == null) {
+			totalCharge = ch1.calNormalCharge();
+		}
+		else if (ch1 == null && ch2 != null) {
+			totalCharge = ch2.calNormalCharge();
+		}
+		else if(ch1 != null && ch2 != null) {
+			totalCharge = ch1.calNormalCharge() + ch2.calNormalCharge();
+		}
 		
-		
-		
-		
-		//System.out.println("Total charges: "+ (ch1.calNormalCharge()+ch2.calNormalCharge()));
-		
-		
-		
-
-		
-		
+		//System.out.println("Total charges: "+ totalCharge);
+	
 	}
 	
 	public static void photo() {
 		
 		String opt, choice;
-		int ttlQtt, normalQtt, passportQtt, addOpt;
+		int ttlQtt, normalQtt = 0, passportQtt = 0, addOpt;
+		int checkLoop = 0;
+		double totalCharge = 0;
 		Charge ch3 = null, ch4= null, surcharge = null;
 		boolean error, er;
 		
-		System.out.print("Enter total photos to print: ");
-		ttlQtt = scanner.nextInt();
 		
 		do {
 			error=false;
 			System.out.print("Enter option(C-Normal(4R)/D-Passport): ");
 			opt = scanner.next().toUpperCase();
 			if(opt.equals("C")) {
+				mainOpt1 = "Normal(4R)";
 				System.out.print("Enter quantity in normal(4R): ");
 				normalQtt = scanner.nextInt();
+				mainQtt1 = normalQtt;
 				ch3 = new Charge(opt,normalQtt);
 				
 				do {
@@ -138,6 +161,7 @@ public class Order {
 						er=false;
 					}
 					else if(choice.equals("N")) {
+						System.out.println("Proceeding to billing...");
 						er=false;
 					}
 					else {
@@ -148,8 +172,10 @@ public class Order {
 			}
 			else if(opt.equals("D"))
 			{
+				mainOpt2 = "Passport";
 				System.out.print("Enter quantity in passport: ");
 				passportQtt = scanner.nextInt();
+				mainQtt2 = passportQtt;
 				ch4 = new Charge(opt,passportQtt);
 				
 				do {
@@ -161,6 +187,7 @@ public class Order {
 						er=false;
 					}
 					else if(choice.equals("N")) {
+						System.out.println("Proceeding to billing...");
 						er=false;
 					}
 					else {
@@ -173,19 +200,29 @@ public class Order {
 				System.out.println("Invalid choice. Please enter again.");
 				error=true;
 			}
-			
+			checkLoop++;
 		}while(error);
+		mainCheckLoop = checkLoop;
+		System.out.println("CheckLoop: "+checkLoop);
 		
+		//Calculate total quantity for photos
+		ttlQtt = normalQtt + passportQtt;
 		
 		do {
 			error=false;
 			System.out.println("Additional Option");
 			System.out.println("1. High quality paper");
 			System.out.println("2. Design effect");
+			System.out.println("3. Exit and bill");
+			System.out.print("Enter Option: ");
 			addOpt = scanner.nextInt();
 			
 			if(addOpt==1||addOpt==2) {
 				surcharge = new Charge(addOpt,ttlQtt);
+				error=false;
+			}
+			else if(addOpt==3) {
+				System.out.println("Proceeding to billing...");
 				error=false;
 			}
 			else {
@@ -194,8 +231,33 @@ public class Order {
 			}
 		}while(error);
 		
+		// this part to check the combination of billing
+		// exp: some buy normal and passport without surcharge, some buy normal with surcharge
 		
-		System.out.println("Total charges: "+ (ch3.calNormalCharge()+ch4.calNormalCharge()+surcharge.calSurcharge()));
+		// != null --> inside particular object got something == people buy the option(normal/passport/addOpt)
+		
+		if(ch3 != null && ch4 == null && surcharge == null) {
+			totalCharge = ch3.calNormalCharge();
+		}
+		else if (ch3 == null && ch4 != null && surcharge == null) {
+			totalCharge = ch4.calNormalCharge();
+		}
+		else if (ch3 != null && ch4 != null && surcharge == null) {
+			totalCharge = ch3.calNormalCharge() + ch4.calNormalCharge();
+		}
+		else if (ch3 != null && ch4 == null && surcharge != null) {
+			totalCharge = ch3.calNormalCharge() + surcharge.calSurcharge();
+		}
+		else if (ch3 == null && ch4 != null && surcharge != null) {
+			totalCharge = ch4.calNormalCharge() + surcharge.calSurcharge();
+		}
+		else if(ch3 != null && ch4 != null && surcharge != null) {
+			totalCharge = ch3.calNormalCharge() + ch4.calNormalCharge() + surcharge.calSurcharge();
+		}
+		
+		
+		//System.out.println("Total charges: "+ totalCharge);
 	}
+	
 	
 }
