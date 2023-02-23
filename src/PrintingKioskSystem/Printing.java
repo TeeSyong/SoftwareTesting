f6693302b9ebcc4147fe344976f51dd34abfc8fb
Czooi <126080176+Czooi@users.noreply.github.com>
2023-02-23 18:41:32 +0800

    Update Printing.java

--- src/PrintingKioskSystem/Printing.java
+++ src/PrintingKioskSystem/Printing.java
@@ -3,5 +3,14 @@ package PrintingKioskSystem;
 import java.util.*;
 
 public class Printing {
-
+	public static void main(String[] args) {
+		
+		//Table
+		
+		
+		Order order = new Order();
+		order.document();
+	}
+	
+	
 }
