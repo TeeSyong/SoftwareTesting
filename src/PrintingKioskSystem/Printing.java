0936f0203f4103f0cbb0b713dae3f5ecab4ec3d4
Czooi <126080176+Czooi@users.noreply.github.com>
2023-02-23 11:24:23 +0800

    Update Printing.java
    update

--- src/PrintingKioskSystem/Printing.java
+++ src/PrintingKioskSystem/Printing.java
@@ -8,35 +8,48 @@ private static Scanner scanner;
 	public static void main(String[] args) {
 		scanner = new Scanner(System.in);
 		
-		int choice;
 		boolean error;
-		String order,opt;
-		int qtt,addOpt;
-		
-		
+		String order;
+				
 		do {
-			System.out.print("Enter type of order(D-Document/P-Photo): ");
-			order = scanner.nextLine();
-			if(order.equals("D")){
-				System.out.print("Enter option(A-Black & White/B-Colour): ");
-				opt = scanner.nextLine();
+			System.out.print("Enter type of order(D-Document/P-Photo)(E-exit): ");
+			order = scanner.nextLine().toUpperCase();
+			error = false;
+			switch(order) {
+			case "D": document(); break;
+			case "P": photo(); break;
+			case "E": break;
+			default: error = true; break;
 			}
-			else {
-				System.out.print("Enter option(C-Normal/D-Passport): ");
-				opt = scanner.nextLine();
+			
+			if(error) {
+				System.out.println("Invalid choice. Please enter again");
 			}
-			System.out.print("Enter quantity: ");
-			qtt = scanner.nextInt();
-			System.out.println("Additional Option");
-			System.out.println("1. High quality paper");
-			System.out.println("2. Design effect");
-			addOpt = scanner.nextInt();
 			
-			
+		}while(!order.equals("E"));
+		
+	}
+	
+	public static void document() {
+		
+		
+		if(order.equals("D")){
+			System.out.print("Enter option(A-Black & White/B-Colour): ");
+			opt = scanner.nextLine();
 		}
-		
-		sadasd
-		
+		else {
+			System.out.print("Enter option(C-Normal/D-Passport): ");
+			opt = scanner.nextLine();
+		}
+		System.out.print("Enter quantity: ");
+		qtt = scanner.nextInt();
+		System.out.println("Additional Option");
+		System.out.println("1. High quality paper");
+		System.out.println("2. Design effect");
+		addOpt = scanner.nextInt();
+	}
+	
+	public static void photo() {
 		
 	}
 }
