48cf97c148756c2e10c558f58254c7144281f75f
Czooi <126080176+Czooi@users.noreply.github.com>
2023-04-20 11:44:19 +0800

    Create PrintingDocument.java

---
+++ src/main/java/printing/objects/PrintingDocument.java
@@ -0,0 +1,47 @@
+package printing.objects;
+
+public class PrintingDocument extends IPrintingOrder {
+
+	
+	public PrintingDocument(PrintingOption pOpt,int qtt)
+	{
+		super(PrintingType.Document,pOpt,qtt);
+	}
+	@Override
+	public double getTotal()
+	{
+		double charge=0;
+		if(printingOpt==PrintingOption.BlackWhite)
+		{
+			charge=getBlackAndWhiteCharge();
+		}
+		else if(printingOpt==PrintingOption.Colour)
+		{
+			charge=getColourCharge();
+		}
+		sum=quantity*charge;
+		return sum;
+	}
+	
+	public double getBlackAndWhiteCharge(){
+		if(quantity < 5)
+			return 0.5;
+		else if (quantity >= 5 && quantity <= 10)
+			return 0.4;
+		else if(quantity >= 11 && quantity <= 20)
+			return 0.3;
+		else
+			return 0.2;
+	}
+	
+	public double getColourCharge() {
+		if(quantity < 5)
+			return 1;
+		else if (quantity >= 5 && quantity <= 10)
+			return 0.9;
+		else if(quantity >= 11 && quantity <= 20)
+			return 0.8;
+		else
+			return 0.7;
+	}
+}
