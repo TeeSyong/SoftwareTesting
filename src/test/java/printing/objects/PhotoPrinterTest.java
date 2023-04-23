e8a4cbc37504e8cf003a3cba4e5d4f16f65fd3ee
TszenYong <tszenyong@gmail.com>
2023-04-24 03:04:38 +0800

    Update PhotoPrinterTest.java

--- src/test/java/printing/objects/PhotoPrinterTest.java
+++ src/test/java/printing/objects/PhotoPrinterTest.java
@@ -15,17 +15,20 @@ public class PhotoPrinterTest {
 	@Test
 	public void testQueueRequest()
 	{
+		//Mock the class that contain method of getRequest that will return the array of orders
 		IPrintingOrder orderMock = mock(IPrintingOrder.class);
+		//Mock the class that contain array of orders
 		IPrintingOrder getOrderMock = mock(IPrintingOrder.class);
-		
+		//When getRequest is called, always return the fixed array of orders
 		when(orderMock.getRequest()).thenReturn(orderMock.printingOrders);
-		
+		//Pass the mock to the printer class the contains the queueRequest method
 		PhotoPrinter print = new PhotoPrinter (orderMock);
 		InOrder inOrder = Mockito.inOrder(orderMock);
 		
 		print.queueRequest();
+		//Assert equals the array of orders return from getRequest with Actual array of orders
 		assertEquals(orderMock.getRequest(),getOrderMock.printingOrders);
-		
+		//Verify array of orders used for updateRequest method with actual array of orders
 		inOrder.verify(orderMock).updateRequest(getOrderMock.printingOrders);
 			
 	}
